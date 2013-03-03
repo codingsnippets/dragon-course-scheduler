70a75bc71fe958e47f809037d8a8dc00ffdb43bb
Kevin Huang <kevin@Kevins-MacBook-Pro-5.local>
2013-03-03 12:31:27 -0500

    Adding in some classes for reading data records.

---
+++ branch/dbs/course_info/dbcredential.java
@@ -0,0 +1,22 @@
+package course_info;
+
+public class dbcredential {
+
+    private String ost="cs451dbremote.db.10419053.hostedresource.com";
+    private String dbname= "cs451dbremote";
+    private String dbpass= "S4aw6WRA4u!";
+
+    private String getDbpass() {
+        return dbpass;
+    }
+
+    private String getDbname() {
+        return dbname;
+    }
+
+    private String getOst() {
+        return ost;
+    }
+
+
+}
