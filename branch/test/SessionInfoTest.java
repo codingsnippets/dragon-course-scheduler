15d1dd7fd6ee6573ff9f22abeb60e2305c8b338e
sbk44 <stanbkol@gmail.com>
2013-03-21 19:52:21 -0400

    update

--- branch/test/SessionInfoTest.java
+++ branch/test/SessionInfoTest.java
@@ -1,17 +1,47 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import static org.junit.Assert.*;
+
+
 
 /**
  * Created with IntelliJ IDEA.
- * User: Stan_2
+ * User: Stan Kolakowski
  * Date: 3/21/13
  * Time: 12:35 PM
  * To change this template use File | Settings | File Templates.
+ *
+ *
+ *
  */
+//        System.out.println("Testing method " + name.getMethodName().substring(4) + "() by: ");
+//        /****   Sample New Test Case *********/
+//        System.out.println("\t" + "<what are you passing>... ");
+//
+//        //initialize your vars
+//        Object si = new Object();
+//        si.testMethod(params);
+//        expected =
+//        test = si.gettestMethod()
+//
+//        try{
+//        assertTrue( test.equals(expected) );
+//        System.out.println("Passed!");
+//        } catch(AssertionError e){
+//        System.out.println("Failed!");
+//        }
 public class SessionInfoTest extends Object {
+    @Rule public TestName name = new TestName();
+
+    filterproto fp = new filterproto();
+    Object expected;
+    Object test;
+
     @Before
     public void setUp() throws Exception {
-
+        //si = new SessionInfo();
     }
 
     @Test
@@ -21,17 +51,82 @@ public class SessionInfoTest extends Object {
 
     @Test
     public void testSetConcentration() throws Exception {
+        System.out.println("Testing method " + name.getMethodName().substring(4) + "() by: ");
+
+        /****   New Test Case *********/
+        System.out.println("\t" + "setting a populated concentrations arraylist... ");
+
+        SessionInfo si = new SessionInfo();
+        //si.setConcentration( fp.getConcentration() );
+        expected = new String("Algor");
+        test = si.getConcentration().get(0);
+
+        try{
+            assertTrue(test.equals(expected));
+            System.out.println("Passed!");
+        } catch(AssertionError e){
+            System.out.println("Failed!");
+        }
+
+        /****   New Test Case *********/
+        System.out.println("\t" + "passing a null... ");
+
+        si = new SessionInfo();
+        si.setConcentration( null );
+        expected = null;
+        test = si.getConcentration();
+
+        try{
+            assertTrue(test == expected);
+            System.out.println("Passed!");
+        } catch(AssertionError e){
+            System.out.println("Failed!");
+        }
 
     }
 
     @Test
     public void testSetMajor() throws Exception {
+        System.out.println("Testing method " + name.getMethodName().substring(4) + "() by: ");
+
+        /****   New Test Case *********/
+        System.out.println("\t" + "setting a String... ");
+
+        SessionInfo si = new SessionInfo();
+        expected = new String("CS");
+
+        si.setMajor("CS");
+        String test = si.getMajor();
+
+        try{
+            assertTrue(test.equals(expected));
+            System.out.println("Passed!\n");
+        } catch(AssertionError e){
+           System.out.println("Failed!\n");
+        }
 
     }
 
     @Test
     public void testSetTermOffer() throws Exception {
+        System.out.println("Testing method " + name.getMethodName().substring(4) + "() by: ");
 
+        /****   New Test Case *********/
+        System.out.println("\t" + "setting populated arrayList... ");
+
+        SessionInfo si = new SessionInfo();
+        expected = new String("Korman 137");
+//        System.out.print("TYPE: " + fp.getCourses().getClass().toString());
+//        si.setTermOffer( fp.getCourses() );
+
+        String test = si.getTermOffer().get(0).getLocation();
+
+        try{
+            assertTrue(test.equals(expected));
+            System.out.println("Passed!\n");
+        } catch(AssertionError e){
+            System.out.println("Failed!\n");
+        }
     }
 
     @Test
@@ -53,4 +148,5 @@ public class SessionInfoTest extends Object {
     public void testRemoveClass() throws Exception {
 
     }
+
 }
