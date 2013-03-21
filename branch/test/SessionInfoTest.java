25473fc693f79ecf53de3148dd30237f4a99c24e
sbk44 <stanbkol@gmail.com>
2013-03-21 12:40:56 -0400

    test source folder

---
+++ branch/test/SessionInfoTest.java
@@ -0,0 +1,56 @@
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stan_2
+ * Date: 3/21/13
+ * Time: 12:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SessionInfoTest extends Object {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testSetlastAction() throws Exception {
+
+    }
+
+    @Test
+    public void testSetConcentration() throws Exception {
+
+    }
+
+    @Test
+    public void testSetMajor() throws Exception {
+
+    }
+
+    @Test
+    public void testSetTermOffer() throws Exception {
+
+    }
+
+    @Test
+    public void testSetClasses() throws Exception {
+
+    }
+
+    @Test
+    public void testSetTerm() throws Exception {
+
+    }
+
+    @Test
+    public void testAddClass() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveClass() throws Exception {
+
+    }
+}
